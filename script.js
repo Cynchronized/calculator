b3c27e5f596c41f4a2e6e55164412c5edfc64ed6
Cynchronized <austin.thieu123@gmail.com>
2023-08-08 16:55:30 -0600

    Add buttons to HTML layout

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let firstNumber;
+let operator;
+let secondNumber
+
+
+// Basic math operators
 const add = (a, b) => a + b
 
 const subtract = (a, b) => a - b
@@ -5,3 +11,19 @@ const subtract = (a, b) => a - b
 const multiply = (a, b) => a * b
 
 const divide = (a, b) => a / b
+
+const operate = function(operate, firstNum, secondNum) {
+    switch(operate) {
+        case "add":
+            add(firstNum, secondNum)
+            break
+        case "subtract":
+            subtract(firstNum, secondNum)
+            break
+        case "multiply":
+            multiply(firstNum, secondNum)
+            break
+        case "divide":
+            divide(firstNum, secondNum)
+    }
+}
