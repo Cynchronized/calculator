6b7f4116013f151874ffb5614f62679e6958a416
Cynchronized <austin.thieu123@gmail.com>
2023-08-08 22:56:57 -0600

    add clear and operate functio

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
-let firstNumber;
-let operator;
-let secondNumber
+let firstOperand = ''
+let secondOperand = ''
+let operator = undefined
 
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operator]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const clearAllButton = document.querySelector('[data-clear]')
+const equationTextElement = document.querySelector('[data-equation]')
+const resultTextElement = document.querySelector('[data-result]')
+
+
+const clear = function () {
+    equationTextElement.textContent = ''
+    resultTextElement.textContent = 0
+    resetValues
+}
+
+const resetValues = function () {
+    firstOperand = ''
+    secondOperand = ''
+    operator = undefined
+}
+
+const deleteNumber = function() {
+
+}
+
+const appendNumber = function(number) {
+    if(resultTextElement.textContent == '0') {
+        resetDisplay()
+    }
+    if(number === '.' && resultTextElement.textContent.includes('.')) return
+    resultTextElement.textContent += number
+}
+
+const chooseOperation = function(operation) {
+    firstOperand = resultTextElement.textContent
+    if(secondOperand !== '') compute
+    if(firstOperand === '') return
+    operator = operation
+    equationTextElement.textContent = `${firstOperand} ${operator}`
+    resultTextElement.textContent = ""
+}
+
+const resetDisplay = function() {
+    resultTextElement.textContent = ''
+
+}
 
 // Basic math operators
 const add = (a, b) => a + b
@@ -12,18 +58,46 @@ const multiply = (a, b) => a * b
 
 const divide = (a, b) => a / b
 
-const operate = function(operate, firstNum, secondNum) {
-    switch(operate) {
-        case "add":
-            add(firstNum, secondNum)
+const operate = function() {
+    let result
+    secondOperand = resultTextElement.textContent
+    const a = parseFloat(firstOperand)
+    const b = parseFloat(secondOperand)
+            console.log('hello')
+
+    console.log(a)
+    console.log(b)
+
+    switch(operator) {
+        case '+':
+            result = add(a, b)
             break
-        case "subtract":
-            subtract(firstNum, secondNum)
+        case '-':
+            result = subtract(a, b)
             break
-        case "multiply":
-            multiply(firstNum, secondNum)
+        case 'x':
+            result = multiply(a, b)
             break
-        case "divide":
-            divide(firstNum, secondNum)
+        case "÷":
+            result = divide(a, b)
     }
+    resultTextElement.textContent = result
+    equationTextElement.textContent = `${firstOperand} ${operator} ${secondOperand}`
+    resetValues
 }
+
+clearAllButton.addEventListener('click', clear)
+
+equalsButton.addEventListener('click', operate)
+
+operationButtons.forEach((button) =>
+    button.addEventListener('click', () => {
+        chooseOperation(button.textContent)
+    })
+)
+
+numberButtons.forEach((button) =>
+    button.addEventListener('click', () => {
+        appendNumber(button.textContent)
+    })
+)
